4863f00e4cae515498cd7ff4ad9d010b14c86c02
Zena-park <zena.p@onther.io>
2021-05-17 20:11:13 +0900

    deploy stakeTON to rinkeby

--- scripts/save_deployed_file.js
+++ scripts/save_deployed_file.js
@@ -1,5 +1,5 @@
 const fs = require("fs");
-
+/*
 module.exports = function (network, deployed) {
   if (!fs.existsSync(`../deployed.${network}.json`)) {
     fs.writeFileSync(
@@ -17,3 +17,17 @@ module.exports = function (network, deployed) {
     JSON.stringify(deployed, null, 2)
   );
 };
+*/
+
+module.exports = function (network, deployed) {
+  if (!fs.existsSync(`deployed.${network}.json`)) {
+    fs.writeFileSync(`deployed.${network}.json`, '{}', { flag: 'w' }, function (err) {
+      if (err) throw err;
+    });
+  }
+
+  let data = JSON.parse(fs.readFileSync(`deployed.${network}.json`).toString());
+  data[deployed.name] = deployed.address;
+
+  fs.writeFileSync(`deployed.${network}.json`, JSON.stringify(data, null, 2))
+}
