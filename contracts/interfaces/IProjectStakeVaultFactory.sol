351d61bf07bb616ce27a4aa216d3f0914ca95929
Zena-park <zena.p@onther.io>
2021-05-23 12:53:46 +0900

    refactor: delete project

--- contracts/interfaces/IProjectStakeVaultFactory.sol
+++
@@ -1,6 +0,0 @@
-//SPDX-License-Identifier: Unlicense
-pragma solidity ^0.7.6;
-
-interface IProjectStakeVaultFactory {
-    function deploy(address project, address token) external returns (address);
-}
