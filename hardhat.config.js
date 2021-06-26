15b128eaf800ba21713d18ac77fb5568021830e5
SEFI2 <kadirpili@gmail.com>
2021-06-26 17:37:13 +0300

    Refactor config file

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,8 +1,8 @@
 require("@nomiclabs/hardhat-ethers");
-require('@openzeppelin/hardhat-upgrades');
+require("@openzeppelin/hardhat-upgrades");
 require("@nomiclabs/hardhat-etherscan");
 require("@nomiclabs/hardhat-waffle");
-require('dotenv').config()
+require("dotenv").config();
 
 require("./tasks/uniswap-v3-approve-erc20-task");
 require("./tasks/uniswap-v3-create-pool-task");
@@ -11,7 +11,11 @@ require("./tasks/uniswap-v3-mint-position-task");
 require("./tasks/uniswap-v3-swap-task");
 require("./tasks/view-tasks");
 
-const { RINKEBY_UNISWAP_V3_ACCOUNT_PK1, RINKEBY_UNISWAP_V3_ACCOUNT_PK2, RINKEBY_UNISWAP_V3_ACCOUNT_PK3 } = process.env;
+const {
+  RINKEBY_UNISWAP_V3_ACCOUNT_PK1,
+  RINKEBY_UNISWAP_V3_ACCOUNT_PK2,
+  RINKEBY_UNISWAP_V3_ACCOUNT_PK3,
+} = process.env;
 task("accounts", "Prints the list of accounts", async () => {
   const accounts = await ethers.getSigners();
   // const prov = await ethers.getDefaultProvider();
@@ -23,51 +27,51 @@ task("accounts", "Prints the list of accounts", async () => {
   }
 });
 module.exports = {
-  defaultNetwork: "rinkeby",
+  defaultNetwork: "localhost",
   networks: {
     localhost: {
-      gas: 9500000,
-      gasMultiplier: 100,
-      blockGasLimit: 124500000,
-    },
+      gas: 12450000,
+      // gasMultiplier: 100,
+      blockGasLimit: 12450000,
+    } /*
     hardhat: {
       accounts: {
         mnemonic: process.env.MNEMONIC_HARDHAT,
         count: 30,
         initialIndex: 0,
-        accountsBalance: '10000000000000000000000',
+        accountsBalance: "10000000000000000000000",
       },
       chainId: 31337,
-    },
+    }, */,
     rinkeby: {
       url: `https://rinkeby.infura.io/v3/${process.env.InfuraKey}`,
       accounts: [
-          `${RINKEBY_UNISWAP_V3_ACCOUNT_PK1}`,
-          `${RINKEBY_UNISWAP_V3_ACCOUNT_PK2}`,
-          `${RINKEBY_UNISWAP_V3_ACCOUNT_PK3}`
-          ],
-      gasMultiplier: 1.25
-    }
+        `${RINKEBY_UNISWAP_V3_ACCOUNT_PK1}`,
+        `${RINKEBY_UNISWAP_V3_ACCOUNT_PK2}`,
+        `${RINKEBY_UNISWAP_V3_ACCOUNT_PK3}`,
+      ],
+      gasMultiplier: 1.25,
+    },
   },
   etherscan: {
-    apiKey: `${process.env.APIKey}`
+    apiKey: `${process.env.APIKey}`,
   },
   solidity: {
     version: "0.7.6",
     settings: {
       optimizer: {
         enabled: true,
-        runs: 100
-      }
-    }
+        runs: 1000,
+      },
+    },
   },
   paths: {
     sources: "./contracts",
     tests: "./test",
     cache: "./cache",
-    artifacts: "./artifacts"
+    artifacts: "./artifacts",
   },
   mocha: {
-    timeout: 10000000
-  }
+    timeout: 10000000,
+  },
 };
