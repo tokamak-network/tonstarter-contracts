6106bef73d521b527ea359b26870af7428c6eeaa
Zena-park <zena.p@onther.io>
2021-07-12 10:24:45 +0900

    fix: add createVault2 in Stake2Logic

--- contracts/stake/Stake2Logic.sol
+++ contracts/stake/Stake2Logic.sol
@@ -26,4 +26,69 @@ contract Stake2Logic is StakeProxyStorage, AccessibleCommon {
         return IERC20(tos).balanceOf(target);
     }
 
+
+    /// @dev create vault2
+    /// @param _cap  allocated reward amount
+    /// @param _rewardPerBlock  the reward per block
+    /// @param _phase  phase of TOS platform
+    /// @param _vaultName  vault's name's hash
+    /// @param _stakeType  it's 2, StakeUniswapV3 staking type
+    /// @param _uniswapInfo  npm, poolFactory, token0, token1
+    /// @param _name   name
+    function createVault2(
+        uint256 _cap,
+        uint256 _rewardPerBlock,
+        uint256 _phase,
+        bytes32 _vaultName,
+        uint256 _stakeType,
+        address[4] memory _uniswapInfo,
+        string memory _name
+    ) external override onlyOwner
+        nonZeroAddress(address(stakeVaultFactory))
+        nonZeroAddress(_uniswapInfo[0])
+        nonZeroAddress(_uniswapInfo[1])
+        nonZeroAddress(_uniswapInfo[2])
+        nonZeroAddress(_uniswapInfo[3])
+    {
+        require(_phase == 2, "Stake1Logic: unsupported phase in vault2");
+
+        uint256 stakeType = _stakeType;
+        uint256 cap = _cap;
+        uint256 rewardPerBlock = _rewardPerBlock;
+
+        address vault =
+            stakeVaultFactory.create2(
+                _phase,
+                [tos, address(stakeFactory)],
+                [stakeType, cap, rewardPerBlock],
+                _name,
+                address(this)
+            );
+        require(vault != address(0), "Stake1Logic: vault2 is zero");
+
+        uint256 phase = _phase;
+        bytes32 vaultName = _vaultName;
+
+        stakeRegistry.addVault(vault, phase, vaultName);
+        emit CreatedVault(vault, _uniswapInfo[0], cap);
+
+        address[4] memory _addr = [tos,vault, address(0), address(0)];
+        address _contract =
+            stakeFactory.create(
+                stakeType,
+                _addr,
+                address(stakeRegistry),
+                [cap, rewardPerBlock, 0]
+            );
+        require(_contract != address(0), "Stake1Logic: vault2 deploy fail");
+
+        address[4] memory uniswapInfo = _uniswapInfo;
+        IIStakeUniswap2(_contract).setPool(uniswapInfo);
+        IStake2Vault(vault).setStakeAddress(_contract);
+        stakeRegistry.addStakeContract(address(vault), _contract);
+
+        emit CreatedStakeContract(address(vault), _contract, phase);
+
+    }
+
 }
