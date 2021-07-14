1ccd736258aa92316071ce993bb530685440708c
Zena-park <zena.p@onther.io>
2021-07-15 08:27:42 +0900

    Update deploy_6_claimTOS_liquidityMiningVault.js

--- scripts/deploy_6_claimTOS_liquidityMiningVault.js
+++ scripts/deploy_6_claimTOS_liquidityMiningVault.js
@@ -26,11 +26,11 @@ async function deployMain(defaultSender) {
   const liquidityMiningVault = new ethers.Contract(process.env.LiquidityMiningVault, SimpleVault1.abi, ethers.provider);
   console.log("Deploying contracts with the account:", deployer.address);
 
-  let _isAdmin = await liquidityMiningVault.isAdmin(user2.address);
-  console.log("liquidityMiningVault isAdmin:  ", user2.address, _isAdmin );
+  let _isAdmin = await liquidityMiningVault.isAdmin(deployer.address);
+  console.log("liquidityMiningVault isAdmin:  ", deployer.address, _isAdmin );
 
   if(_isAdmin){
-    let tx = await liquidityMiningVault.connect(user2).claimTOS(
+    let tx = await liquidityMiningVault.connect(deployer).claimTOS(
         process.env.PHASE1_TON_VAULT_ADDRESS,
         utils.parseUnits(process.env.PHASE1_TON_ALLOCATED + "." + "0".repeat(18))
     );
