1f7b2a96c78e39c37d811b06d228cde083e67f2f
HyukSang <zzooppii@gmail.com>
2022-05-16 18:30:13 +0900

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -7,24 +7,25 @@ require("hardhat-gas-reporter");
 require('dotenv/config');
 
 require("dotenv").config();
-require("./tasks/get-lock-tos-info-task");
-require("./tasks/deploy-lock-tos-task");
-require("./tasks/uniswap-v3-approve-erc20-task");
-require("./tasks/uniswap-v3-create-pool-task");
-require("./tasks/uniswap-v3-increase-liquidity-task");
-require("./tasks/uniswap-v3-mint-position-task");
-require("./tasks/uniswap-v3-swap-task");
-require("./tasks/view-tasks");
-require("./tasks/abi-retriever");
+
+// require("./tasks/get-lock-tos-info-task");
+// require("./tasks/deploy-lock-tos-task");
+// require("./tasks/uniswap-v3-approve-erc20-task");
+// require("./tasks/uniswap-v3-create-pool-task");
+// require("./tasks/uniswap-v3-increase-liquidity-task");
+// require("./tasks/uniswap-v3-mint-position-task");
+// require("./tasks/uniswap-v3-swap-task");
+// require("./tasks/view-tasks");
+// require("./tasks/abi-retriever");
 
 // require("./tasks-rewardprogram/testcase");
 
-const {
-  RINKEBY_UNISWAP_V3_ACCOUNT_PK1,
-  RINKEBY_UNISWAP_V3_ACCOUNT_PK2,
-  RINKEBY_UNISWAP_V3_ACCOUNT_PK3,
-} = process.env;
-const { ACCOUNT0_PK, ACCOUNT1_PK, TONSTARTER_DEPLOYER_PK } = process.env;
+// const {
+//   RINKEBY_UNISWAP_V3_ACCOUNT_PK1,
+//   RINKEBY_UNISWAP_V3_ACCOUNT_PK2,
+//   RINKEBY_UNISWAP_V3_ACCOUNT_PK3,
+// } = process.env;
+// const { ACCOUNT0_PK, ACCOUNT1_PK, TONSTARTER_DEPLOYER_PK } = process.env;
 
 // const { ACCOUNT0_PK, ACCOUNT1_PK, ACCOUNT2_PK } = process.env;
 
