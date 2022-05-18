0b82a50d40e1f6685e6771776c1a8435914cf3e8
Zena-park <zena.p@onther.io>
2022-05-18 21:19:06 +0900

    TSV2-87. apply staking

--- tasks/stos-holders-info.js
+++ tasks/stos-holders-info.js
@@ -1,27 +1,30 @@
 const fs = require('fs');
-// const {
-//   getUpdateTONStakedAmount,
-//   getUpdateStakersList,
-//   getStakersList,
-//   getLayer2List,
-//   getTONStakedAmount,
-//   erc20RecorderMint,
-//   concatStakers,
-//   getStakersListOfLayers,
-//   getStakersListOfLayersChanged,
-//   getAutocoinageData,
-//   updateAutocoinageData,
-//   getTotalSupplyLayer2,
-//   getBalanceLayer2Account,
-//   syncAutocoinageData,
-//   getTotalSupplyLayer2WithAutyoCoinageSnapshot,
-//   getBalanceLayer2AccountWithAutyoCoinageSnapshot,
-//   viewAutoCoinageSnapshotAddress
-// } = require("../test/helpers/ton-stakers");
+const {
+  getLockTosHolderList,
+  getStosBalances,
+  applyStosBalances
+} = require("../test/lockTos/tosv2-migration");
 
 task("get-stos-holder-list", "Retrieve sTos holder list into a file")
     .addParam("lockTosAddress", "LockTOS Address")
     .setAction(async ({ lockTosAddress }) => {
-      await getSTOSHolderList(lockTosAddress);
+      await getLockTosHolderList(lockTosAddress);
+    })
+
+
+
+task("get-stos-balances-accounts", "Retrieve sTos balance of accounts into a file")
+    .addParam("lockTosAddress", "LockTOS Address")
+    .addParam("blockNumber", "blockNumber ")
+    .setAction(async ({ lockTosAddress, blockNumber }) => {
+      await getStosBalances(lockTosAddress, blockNumber);
+    })
+
+
+task("apply-stos-balances-accounts", "Apply sTos balance of accounts into Staking")
+    .addParam("lockTosAddress", "LockTOS Address")
+    .addParam("stakeAddress", "stake Address ")
+    .setAction(async ({ lockTosAddress, stakeAddress }) => {
+      await applyStosBalances(lockTosAddress, stakeAddress);
     })
 
